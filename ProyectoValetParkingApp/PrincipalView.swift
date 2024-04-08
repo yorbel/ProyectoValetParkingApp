6776cde5be7c694ffb90922e1bbfbfad0df0e3dd
yorbel <yorbel8perez@gmail.com>
2024-04-08 17:40:41 -0400

    HS y HS

--- ProyectoValetParkingApp/PrincipalView.swift
+++ ProyectoValetParkingApp/PrincipalView.swift
@@ -88,7 +88,7 @@ struct PrincipalView: View {
                
                 VStack(alignment: .leading){
 
-                    ForEach(i in 1...ceil(tickets.count/3)){
+                    ForEach( i in stride(from: 1, to: ceil(tickets.count/3), by: 1) ){
                         HStack{
                             // ForEach(tickets, id: \.id){ ticket in
                                 Label("**\(i)**", systemImage: "car")
