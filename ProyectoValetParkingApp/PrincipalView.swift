9236ba39dd6a10322bf71053f86a17b362f64a21
yorbel <yorbel8perez@gmail.com>
2024-04-11 11:03:20 -0400

    color rgb

--- ProyectoValetParkingApp/PrincipalView.swift
+++ ProyectoValetParkingApp/PrincipalView.swift
@@ -110,8 +110,8 @@ struct PrincipalView: View {
                                     ForEach(lista_tickets_solicitados.tickets_solicitados[(3*i)...( ((3*i)+2) > (lista_tickets_solicitados.tickets_solicitados.count-1) ? (lista_tickets_solicitados.tickets_solicitados.count-1) : ((3*i)+2) )], id: \.id){ ticket_solicitado in
                                         Label("**\(ticket_solicitado.ticket)**", systemImage: ticket_solicitado.vehiculo_solicitado == "SI" ? "clock" : "car" )
                                             .foregroundColor(.white)
-                                            .padding(9)
-                                            .background( ticket == ticket_solicitado.ticket ? Color(red: 0, green: 83.1, blue: 16.5) : ( ticket_solicitado.vehiculo_solicitado == "SI" ? Color(red: 196, green: 0, blue: 0) : Color(red: 0, green: 0, blue: 159) ) )
+                                            .padding(6)
+                                            .background( ticket == ticket_solicitado.ticket ? Color(red: 0, green: 212 / 255 , blue: 42 / 255) : ( ticket_solicitado.vehiculo_solicitado == "SI" ? Color(red: 196, green: 0, blue: 0) : Color(red: 0, green: 0, blue: 159) ) )
                                             .cornerRadius(15)
                                             .onTapGesture{
                         
