7e9cd3877e7ff94f1a5dd4048602203304d0df39
yorbel <yorbel8perez@gmail.com>
2024-04-16 15:32:08 -0400

    preparando notificacion

--- ProyectoValetParkingApp/PrincipalView.swift
+++ ProyectoValetParkingApp/PrincipalView.swift
@@ -9,6 +9,49 @@ import SwiftUI
 import CodeScanner
 import AVFoundation
 import SDWebImageSwiftUI
+import FirebaseCore
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate  {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+        FirebaseApp.configure()
+
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
+        application.registerForRemoteNotifications()
+
+        Messaging.messaging().delegate = self
+        Messaging.messaging().subscribe(toTopic: "lugar35")
+
+        return true
+      
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcm = Messaging.messaging().fcmToken {
+            print("fcm", fcm)
+        }
+    }
+
+    // CUANDO EL USUARIO ESTA VIENDO LA PANTALLA
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([[.alert, .badge, .sound]])
+    }
+
+    // CUANDO EL USUARIO NO ESTA VIENDO LA PANTALLA
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+}
 
 class ListaTicketModel: ObservableObject {
 
@@ -38,6 +81,8 @@ struct PrincipalView: View {
     @State private var mostrar_alerta = false
 
     @State private var animar_imagen_alerta : Bool  = true
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some View {
         ZStack{
