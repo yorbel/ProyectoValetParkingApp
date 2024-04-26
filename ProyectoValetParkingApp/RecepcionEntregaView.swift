b5f4529c171ea1c1f652c75d72cbe7e149e85fc1
yorbel <yorbel8perez@gmail.com>
2024-04-26 18:58:09 -0400

    arreglando socket 4

--- ProyectoValetParkingApp/RecepcionEntregaView.swift
+++ ProyectoValetParkingApp/RecepcionEntregaView.swift
@@ -26,7 +26,7 @@ struct RecepcionEntregaView: View {
 
     @State private var socket: SocketIOClient!
 
-    let manager = SocketManager(socketURL:  URL(string: Globales.url + "/?ticket=050607&lugar_id=\(34)")!, config: [.log(true), .compress] )
+    let manager = SocketManager(socketURL:  URL(string: Globales.url)!, config: [.log(true), .compress, .connectParams(["ticket": "050607", "lugar_id": 34])] )
        
     var body: some View {
         ZStack{
