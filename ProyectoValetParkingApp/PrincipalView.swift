251ff2cf277c1e0f6ad6fa2b670c5ce024e2f31f
yorbel <yorbel8perez@gmail.com>
2024-04-30 22:39:06 -0400

    noti 2

--- ProyectoValetParkingApp/PrincipalView.swift
+++ ProyectoValetParkingApp/PrincipalView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 import CodeScanner
 import AVFoundation
 import SDWebImageSwiftUI
-import FirebaseCore
-import FirebaseMessaging
-import UserNotifications
+
 
 extension URL {
   subscript(name: String) -> String? {
@@ -20,46 +18,6 @@ extension URL {
   }
 }
 
-class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate  {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    
-        FirebaseApp.configure()
-
-        UNUserNotificationCenter.current().delegate = self
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
-        application.registerForRemoteNotifications()
-
-        Messaging.messaging().delegate = self
-        Messaging.messaging().subscribe(toTopic: "lugar35")
-
-        return true
-      
-    }
-
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-    }
-    
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        if let fcm = Messaging.messaging().fcmToken {
-            print("fcm", fcm)
-        }
-    }
-
-    // CUANDO EL USUARIO ESTA VIENDO LA PANTALLA
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([[.alert, .badge, .sound]])
-    }
-
-    // CUANDO EL USUARIO NO ESTA VIENDO LA PANTALLA
-
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        completionHandler()
-    }
-    
-}
 
 class ListaTicketModel: ObservableObject {
 
@@ -91,8 +49,6 @@ struct PrincipalView: View {
     @State private var mostrar_alerta = false
 
     @State private var animar_imagen_alerta : Bool  = true
-
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     @FocusState private var is_focused: Bool
     
--- ProyectoValetParkingApp/ProyectoValetParkingAppApp.swift
+++ ProyectoValetParkingApp/ProyectoValetParkingAppApp.swift
@@ -6,9 +6,56 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
+
+
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate  {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+        FirebaseApp.configure()
+
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
+        application.registerForRemoteNotifications()
+
+        Messaging.messaging().delegate = self
+        Messaging.messaging().subscribe(toTopic: "lugar35")
+
+        return true
+      
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcm = Messaging.messaging().fcmToken {
+            print("fcm", fcm)
+        }
+    }
+
+    // CUANDO EL USUARIO ESTA VIENDO LA PANTALLA
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([[.alert, .badge, .sound]])
+    }
+
+    // CUANDO EL USUARIO NO ESTA VIENDO LA PANTALLA
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+}
 
 @main
 struct ProyectoValetParkingAppApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
